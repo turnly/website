cd53cc48e8dcdec5ad6937547a8e60bb6c81da87
Efra Peralta <efra23peralta@gmail.com>
2022-09-01 23:24:30 -0400

    chore: add website files

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// example of simple includes for js
+//=include lib/jquery.min.js
+//=include lib/slick.min.js
+//=include lib/svg4everybody.min.js
+//=include lib/jquery.magnific-popup.min.js
+//=include lib/aos.js
+//=include lib/rellax.min.js
+//=include lib/simpleParallax.min.js
